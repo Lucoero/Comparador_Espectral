2ebf83a1ae43ce8a5cc670c854aa8d43c3980b2f
KurtGoumas <121434699+KurtGoumas@users.noreply.github.com>
2026-03-11 20:28:32 +0100

    Merge branch 'main' of https://github.com/Lucoero/Practica_I_Astro

--- Codigos/normalizar.py
+++ Codigos/normalizar.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env jupyter
 import numpy as np
 import scipy as scp
+from scipy.signal import medfilt
+from scipy.signal import savgol_filter
+from scipy.signal import spline_filter
+
+
+def filtro(wave, flux, params, tipo='med'):
+    if tipo == 'med':
+        return medfilt(flux, *params)
+    elif tipo == 'sg':
+        return savgol_filter(flux, *params)
+    elif tipo == 'spline':
+        return spline_filter(flux, params)
